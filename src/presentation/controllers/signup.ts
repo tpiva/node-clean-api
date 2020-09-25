d491ae50108e28a57f8dc7b65501abb077b16b79
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2020-09-25 15:32:59 -0300

    ensure signupcontroller.handler implement correct interfaces

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,5 +1,7 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignUpController {
-  handler (httpRequest: any): any {
+  handler (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
