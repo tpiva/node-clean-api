c471588658c055be42df23305ef21251fb9ab17e
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2020-11-24 19:38:25 -0300

    feat: ensure accountmongorepository return an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountModel } from '@/domain/usecases/addAccount'
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = result.ops[0]
+    const { _id, ...accountWithoutID } = account
+    return Object.assign({}, accountWithoutID, { id: _id })
+  }
+}
