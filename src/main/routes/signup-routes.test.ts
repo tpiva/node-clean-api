1cc9949310c6b11d6488d09a739b8ccb96266dc5
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2022-10-27 19:11:21 -0300

    feat: connect signup with mongo db

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,16 @@
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import request from 'supertest'
 import app from '../config/app'
 
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
   test('should return an account with success', async () => {
     await request(app).post('/api/signup')
       .expect(200)
