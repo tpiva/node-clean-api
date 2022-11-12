179cc9be7a217c19c4ba263763ad519aa64acb49
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2022-11-12 19:29:47 -0300

    refactor: remove duplicated code in test

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,25 +1,39 @@
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
+import { AccountModel } from '../../domain/models/account'
+import { ok } from '../../presentation/helpers/http'
 
 interface SutTypes {
   sut: LogControllerDecorator
   controllerStub: Controller
 }
 
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'any_name',
+    email: 'any_email@email.com',
+    password: 'any_password',
+    passwordConfirmation: 'any_password'
+  }
+})
+
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
     async handler (httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        body: {
-          name: 'Thiago'
-        },
-        statusCode: 200
-      }
-      return new Promise(resolve => resolve(httpResponse))
+      return new Promise(resolve => resolve(ok(makeFakeAccount())))
     }
   }
   return new ControllerStub()
 }
+
+const makeFakeAccount = (): AccountModel => (
+  {
+    id: 'valid_id',
+    name: 'valid_name',
+    email: 'valid_email@gmail.com',
+    password: 'valid_password'
+  }
+)
 
 const makeSut = (): SutTypes => {
   const controllerStub = makeController()
@@ -32,17 +46,9 @@ const makeSut = (): SutTypes => {
 
 describe('Log Controller Decorator', () => {
   it('should call controller handle', async () => {
-    const httpRequest = {
-      body: {
-        email: 'any_email@email.com',
-        name: 'any_email',
-        password: '123',
-        passwordConfirmation: '123'
-      }
-    }
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handler')
-    await sut.handler(httpRequest)
-    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+    await sut.handler(makeFakeRequest())
+    expect(handleSpy).toHaveBeenCalledWith(makeFakeRequest())
   })
 })
