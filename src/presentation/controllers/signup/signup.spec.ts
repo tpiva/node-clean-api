3ae2d1c33395adebb69535286aea9ddcb25bdb33
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2022-12-15 14:26:01 -0300

    feat: ensure SignupController returns 400 if Validation fails

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -211,4 +211,11 @@ describe('Signup Controller', () => {
     await sut.handler(httpRequest)
     expect(validateSpy).toHaveBeenLastCalledWith(httpRequest.body)
   })
+
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpResponse = await sut.handler(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
 })
