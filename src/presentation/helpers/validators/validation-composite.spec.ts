d81c40bdaaa2c3a0f23df9f8ed79462554948961
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2022-12-19 15:51:45 -0300

    test: ensure ValidationComposite not return if validation succeds

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -40,4 +40,10 @@ describe('ValidationComposite', () => {
     const error = sut.validate({ field: 'any_value' })
     expect(error).toEqual(new Error())
   })
+
+  it('Should not return if validation succeds', () => {
+    const { sut } = makeSut()
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toBeFalsy()
+  })
 })
