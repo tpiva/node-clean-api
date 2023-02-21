3ae85777ebca73a1f93a192cab1e90f526739934
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-02-21 20:05:58 -0300

    feat: ensure AddSurveyController return 204 if addSurvey usecase success

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,4 +1,4 @@
-import { badRequest, serverError } from '../../../helpers/http/http-helper'
+import { badRequest, noContent, serverError } from '../../../helpers/http/http-helper'
 import { HttpRequest, Validation } from '../../../protocols'
 import { AddSurveyController } from './add-survey-controller'
 import { AddSurvey, AddSurveyModel } from './add-survey-controller-protocols'
@@ -77,5 +77,11 @@ describe('Add Surver Controller', () => {
     jest.spyOn(addSurveyStub, 'add').mockResolvedValueOnce(new Promise((resolve, reject) => reject(new Error())))
     const httpResponse = await sut.handler(makeHttpRequest())
     expect(httpResponse).toEqual(serverError(new Error()))
+  })
+
+  test('Should return 204 on success', async () => {
+    const { sut } = makeSut()
+    const httpResponse = await sut.handler(makeHttpRequest())
+    expect(httpResponse).toEqual(noContent())
   })
 })
