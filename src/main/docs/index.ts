11b17024a5c895db6c5baa6582b7ba1a3b542a9f
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-09-01 20:32:38 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,30 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from './schemas/account-schema'
+import { loginParamsSchema } from './schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API mongo course',
     version: '1.0.0'
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }, {
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
