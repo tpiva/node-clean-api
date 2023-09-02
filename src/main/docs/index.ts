4d074b58af8bdf063822ff676816301df86b12e4
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-09-01 21:02:00 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,6 @@
 import { loginPath } from './paths/login-path'
-import { accountSchema } from './schemas/account-schema'
-import { loginParamsSchema } from './schemas/login-params-schema'
+import { badRequest, serverError, unauthorized, notFound } from './components'
+import { loginParamsSchema, accountSchema, errorSchema } from './schemas'
 
 export default {
   openapi: '3.0.0',
@@ -8,6 +8,10 @@ export default {
     title: 'Clean Node API',
     description: 'API mongo course',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
   servers: [{
     url: '/api',
@@ -25,6 +29,13 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
--- src/main/docs/paths/login-path.ts
+++ src/main/docs/paths/login-path.ts
@@ -23,19 +23,19 @@ export const loginPath = {
             }
           }
         }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      401: {
+        $ref: '#/components/unauthorized'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
       }
-    //   400: {
-    //     $ref: '#/components/badRequest'
-    //   },
-    //   401: {
-    //     $ref: '#/components/unauthorized'
-    //   },
-    //   404: {
-    //     $ref: '#/components/notFound'
-    //   },
-    //   500: {
-    //     $ref: '#/components/serverError'
-    //   }
     }
   }
 }
