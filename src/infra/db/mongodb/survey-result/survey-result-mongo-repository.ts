2d65837e1b3ae74c86dc9feab89e1d46422b0caf
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-09-16 19:55:01 -0300

    refactor: use builder

--- src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongo-repository.ts
@@ -1,7 +1,6 @@
 import { SaveSurveyResultParams, SaveSurveyResultRepository, SurveyResultModel } from '@/data/usecases/survey-result/save-survey-result/db-save-survey-result-protocols'
-import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
-import { QueryBuilder } from '../helpers/query-builder'
+import { QueryBuilder, MongoHelper } from '../helpers'
 import round from 'mongo-round'
 
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
