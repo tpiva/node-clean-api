b9b5dd828a5218e85648d4d03690d1c042342623
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-10-09 17:48:12 -0300

    feat: ensure load survey route returns 403 if no accessToken is provided

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -75,4 +75,31 @@ describe('Survey Routes', () => {
         .expect(200)
     })
   })
+
+  describe('GET /surveys/:surveyId/results', () => {
+    test('Should return 403 on load survey result without accessToken', async () => {
+      await request(app).get('/api/surveys/any_id/results')
+        .expect(403)
+    })
+
+    // test('Should return 200 on save survey result with accessToken', async () => {
+    //   const accessToken = await makeAccessToken()
+    //   const res = await surveyCollection.insertOne({
+    //     question: 'Question',
+    //     answers: [{
+    //       answer: 'Answer 1',
+    //       image: 'http://image-name.com'
+    //     },{
+    //       answer: 'Answer 2'
+    //     }],
+    //     date: new Date()
+    //   })
+    //   await request(app).put(`/api/surveys/${res.ops[0]._id}/results`)
+    //     .set('x-access-token', accessToken)
+    //     .send({
+    //       answer: 'Answer 1'
+    //     })
+    //     .expect(200)
+    // })
+  })
 })
