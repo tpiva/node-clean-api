ae2fdcda85c524da5ed0d59de53f1331a9eacd4f
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-10-09 17:50:57 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -82,24 +82,21 @@ describe('Survey Routes', () => {
         .expect(403)
     })
 
-    // test('Should return 200 on save survey result with accessToken', async () => {
-    //   const accessToken = await makeAccessToken()
-    //   const res = await surveyCollection.insertOne({
-    //     question: 'Question',
-    //     answers: [{
-    //       answer: 'Answer 1',
-    //       image: 'http://image-name.com'
-    //     },{
-    //       answer: 'Answer 2'
-    //     }],
-    //     date: new Date()
-    //   })
-    //   await request(app).put(`/api/surveys/${res.ops[0]._id}/results`)
-    //     .set('x-access-token', accessToken)
-    //     .send({
-    //       answer: 'Answer 1'
-    //     })
-    //     .expect(200)
-    // })
+    test('Should return 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken()
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [{
+          answer: 'Answer 1',
+          image: 'http://image-name.com'
+        },{
+          answer: 'Answer 2'
+        }],
+        date: new Date()
+      })
+      await request(app).get(`/api/surveys/${res.ops[0]._id}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200)
+    })
   })
 })
