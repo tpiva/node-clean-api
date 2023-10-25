e54abf4b2b6b6f4c707b7a7f1dc45e1ac6f1b75a
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2023-10-25 18:44:44 -0300

    feat: add route to static files

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import setupMiddleware from './middlewares'
 import setupRoutes from './routes'
+import setupStaticFiles from './static-files'
 import express from 'express'
 import setupSwagger from './config-swagger'
 
 const app = express()
+setupStaticFiles(app)
 setupSwagger(app)
 setupMiddleware(app)
 setupRoutes(app)
---
+++ src/main/config/static-files.ts
@@ -0,0 +1,7 @@
+/* eslint-disable n/no-path-concat */
+import express, { Express } from 'express'
+import { resolve } from 'path'
+
+export default (app: Express): void => {
+  app.use('/static', express.static(resolve(__dirname, '../../static')))
+}
