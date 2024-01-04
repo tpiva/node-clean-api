2a14887cd809e9e1a8272bf7844c6372a1b2c013
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2024-01-04 18:56:46 -0300

    feat: setup apollo server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
 import setupMiddleware from './middlewares'
+import setupApolloServer from './apollo-server'
 import setupRoutes from './routes'
 import setupStaticFiles from './static-files'
 import express from 'express'
-import setupSwagger from './config-swagger'
+import setupSwagger from './swagger'
 
 const app = express()
+setupApolloServer(app)
 setupStaticFiles(app)
 setupSwagger(app)
 setupMiddleware(app)
