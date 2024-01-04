86391866b75437d7c52a63259a9f757a4008ffac
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2024-01-04 19:37:29 -0300

    feat: signup mutation

--- src/main/adapters/apollo-server-resolver-adpater.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@/presentation/protocols'
-
-export const adapterResolver = async (controller: Controller, args: any): Promise<any> => {
-  const httpResponse = await controller.handler(args)
-  return httpResponse.body
-}
--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,8 +1,13 @@
-import { adapterResolver } from '@/main/adapters/apollo-server-resolver-adpater'
+import { adapterResolver } from '@/main/adapters/apollo-server-resolver-adapter'
 import { makeLoginController } from '@/main/factories/controllers/login/login/login-controller-factory'
+import { makeSignupController } from '@/main/factories/controllers/login/signup/signup-controller-factory'
 
 export default {
   Query: {
     login: async (parent: any, args: any) => adapterResolver(makeLoginController(), args)
+  },
+
+  Mutation: {
+    signUp: async (parent: any, args: any) => adapterResolver(makeSignupController(), args)
   }
 }
