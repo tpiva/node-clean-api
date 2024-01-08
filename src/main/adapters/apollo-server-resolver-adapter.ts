bf4e79b2071e931cd0aa24989f15b6247521c396
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2024-01-08 17:46:38 -0300

    feat: add survey schema

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,8 +1,9 @@
 import { Controller } from '@/presentation/protocols'
 import { ApolloError, AuthenticationError, ForbiddenError, UserInputError } from 'apollo-server-express'
 
-export const adapterResolver = async (controller: Controller, args: any): Promise<any> => {
-  const httpResponse = await controller.handler(args)
+export const adapterResolver = async (controller: Controller, args?: any): Promise<any> => {
+  const request = { ...(args || {}) }
+  const httpResponse = await controller.handler(request)
   switch (httpResponse.statusCode) {
     case 200:
     case 204: return httpResponse.body
