6468bbc3707aef4ec59ce09f7da753443b9850b9
Thiago Piva Magalhaes <thiago.p.magalhaes@gmail.com>
2024-01-17 18:25:23 -0300

    chore: release 3.0.0

--- src/data/usecases/survey/load-surveys/db-load-surveys.ts
+++ src/data/usecases/survey/load-surveys/db-load-surveys.ts
@@ -2,7 +2,7 @@ import { LoadSurveys, LoadSurveysRepository } from './db-load-surveys-protocols'
 
 export class DbLoadSurveys implements LoadSurveys {
   constructor (private readonly loadSurveyRepository: LoadSurveysRepository) {}
-  async load (accountId: string): Promise<LoadSurveysRepository.Result[]> {
+  async load (accountId: string): Promise<LoadSurveysRepository.Result> {
     return this.loadSurveyRepository.loadAll(accountId)
   }
 }
